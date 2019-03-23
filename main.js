c3e2ffd4e1bd5a608ab06c46c0c660dba951873c
YarHovor <yaroslav.hovorukha@gmail.com>
2019-03-24 01:16:53 +0200

    hw4

---
+++ main.js
@@ -0,0 +1,58 @@
+"use strict";
+
+// Ex.2
+
+var c, d, result;
+
+c = 15;
+d = 2;
+
+result = c + d;
+alert('Задание 2 - Значение переменной result: ' + result);
+
+// Ex.3
+
+var a, b, sum;
+
+a = 10;
+b = 2;
+c=5;
+
+sum = a + b + c;
+alert('Задание 3 - Сумма: ' + sum);
+
+// Ex.4
+
+
+a = 17;
+b = 10;
+c = a-b;
+d=7;
+
+alert('Задание 4 - Результат С: ' + c +' и D: ' + d);
+
+// Ex.5
+
+result = c+d;
+alert('Задание 5 - Значение c+d: ' + result);
+
+//Ex.6
+
+var str = 'Задание 6 - Привет, Мир!';
+
+alert(str);
+
+//Ex.7
+
+var str1='Привет', str2='Мир';
+alert('Задание 7 - '+str1+', '+str2+'!');
+
+//Ex.8
+
+var name='Ярослав';
+alert('Задание 8 - '+'Привет, '+name+'!');
+
+//Ex.9
+
+var age=23;
+alert('Задание 9 - '+'Мне '+age+' лет!');
